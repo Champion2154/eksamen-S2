092bb3d86de245e02236e7a833dc97f280e3696b
Narenboi <73293318+Narenboi@users.noreply.github.com>
2023-05-09 12:07:52 +0200

    Add files via upload

---
+++ Eksamens projekt semester 2/src/test/java/com/example/asdfasd/AsdfasdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asdfasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdfasdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
